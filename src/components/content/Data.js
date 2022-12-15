93a07b2e87286c54720c90218f2ae89d7cf0ea5c
20120373 <letruongkinhthanh@gmail.com>
2022-12-16 02:41:12 +0700

    dashboard

---
+++ src/components/content/Data.js
@@ -0,0 +1,71 @@
+export const UserData = [
+    {
+      id: 1,
+      lastWeek: "3/12",
+      thisWeek: "10/12",
+      lastMonth: "First Week",
+      readerLastWeek: 80000,
+      readerThisWeek: 40000,
+      readerLastMonth: 45000
+
+    },
+    {
+      id: 2,
+      lastWeek: "4/12",
+      thisWeek: "11/12",
+      lastMonth: "Second Week",
+      readerLastWeek: 45677,
+      readerThisWeek: 60000,
+      readerLastMonth: 69000
+
+    },
+    {
+      id: 3,
+      lastWeek: "5/12",
+      thisWeek: "12/12",
+      lastMonth: "Third Week",
+
+      readerLastWeek: 78888,
+      readerThisWeek: 30000,
+      readerLastMonth: 45000
+
+    },
+    {
+      id: 4,
+      lastWeek: "6/12",
+      thisWeek: "13/12",
+      lastMonth: "Forth Week",
+      readerLastMonth: 70000,
+
+      readerLastWeek: 90000,
+      readerThisWeek: 70000
+
+    },
+    {
+      id: 5,
+      lastWeek: "7/12",
+      thisWeek: "14/12",
+
+      readerLastWeek: 4300,
+      readerThisWeek: 10000
+
+    },
+    {
+        id: 6,
+        lastWeek: "8/12",
+        thisWeek: "15/12",
+
+        readerLastWeek: 90000,
+        readerThisWeek: 80000
+
+      },
+      {
+        id: 7,
+        lastWeek: "9/12",
+        thisWeek: "16/12",
+
+        readerLastWeek: 4300,
+        readerThisWeek: 60000
+
+      },
+  ];
